8e8ff081498c396d056bb78f98c079f6bc1e1b9d
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2025-05-19 21:26:37 -0300

    rfatoração de métodos

---
+++ JAVA I/ExerciciosJavaI/src/main/java/com/exercicios/java/exercicio09/backEnd/Calcular.java
@@ -0,0 +1,6 @@
+package com.exercicios.java.exercicio09.backEnd;
+
+public interface Calcular {
+	
+	public int calcular();
+}
---
+++ JAVA I/ExerciciosJavaI/src/main/java/com/exercicios/java/exercicio09/backEnd/VelocException.java
@@ -0,0 +1,10 @@
+package com.exercicios.java.exercicio09.backEnd;
+
+public class VelocException extends Exception {
+
+	public VelocException(String message) {
+		super(message);
+	}
+	
+	
+}
